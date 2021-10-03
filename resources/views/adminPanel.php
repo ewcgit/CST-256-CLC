bc17655b568045cfc2d84860fce9cc36ed4d039a
caseycase5 <caseycase5@gmail.com>
2021-10-03 22:28:09 +0800

    Unneeded. Transferred to Blade template

--- resources/views/adminPanel.php
+++
@@ -1,7 +0,0 @@
-<?php // CST-256 page by team.
-	use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\AdminController;
-
-	$controller = new AdminController();
-	$controller->adminPanel();
-?>
