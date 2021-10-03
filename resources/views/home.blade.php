7c99fdcf393c907ac33fde212edc3ac714408b79
caseycase5 <caseycase5@gmail.com>
2021-10-04 07:30:46 +0800

    Updated

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!-- CST-256 CLC Project Version 1.0 Page Created by Elijah Coverini -->
+
+<?php 
+// A simple login and registration page for users.
+?>
+<html>
+<head>
+<title>Home</title>
+<link href="{{ asset('/css/style.css') }}" rel="stylesheet"> 
+</head>
+	<body>
+		<div class="container">
+		<h1> Welcome </h1> 
+		<a href="login">Login</a>
+		<a href="registration">Register</a>
+	</div>
+</body>
+</html>
---
+++ resources/views/loginFailed.php
@@ -0,0 +1,16 @@
+<?php
+// CST-256 page by team.
+// A simple page for failed logins.
+?>
+
+<html>
+	<head>
+		<title>Login Failed</title>	
+	</head>
+
+	<body>
+		<h1>Login Failed</h1><br><br>
+		
+		<a href="login">Return to Login</a>
+	</body>
+</html>
